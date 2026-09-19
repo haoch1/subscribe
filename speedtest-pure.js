d9bb542e437728dbf0b73793e360999293ef0e91
haoch1 <126861633+haoch1@users.noreply.github.com>
2026-09-19 18:59:44 +0800

    Use Speedtest official namespace

--- speedtest-pure.js
+++ speedtest-pure.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Speedtest Pure
-// @namespace    local.speedtest.center
-// @version      4.0.8
+// @namespace    https://www.speedtest.net/
+// @version      4.0.9
 // @icon         https://www.speedtest.net/favicon.ico
 // @description  精简测速界面，默认单连接；结果 IP 点击显示/隐藏，支持 IPv4/IPv6
 // @match        https://www.speedtest.net/*
